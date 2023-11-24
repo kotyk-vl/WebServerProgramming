e67e820223da1fc2e6f552dfabbb23b96d913c4e
Vladyslav Kotyk <vladkotuk@ukr.net>
2023-11-24 14:54:18 +0200

    Added image helper

---
+++ SageBook.Common/Helpers/ImageHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.Versioning;
+
+namespace SageBook.Common.Helpers
+{
+    public static class ImageHelper
+    {
+        [SupportedOSPlatform("windows")]
+        public static byte[]? ImageToByteArray(Image imageIn)
+        {
+            if (imageIn != null)
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    imageIn.Save(ms, ImageFormat.Jpeg);
+                    return ms.ToArray();
+                }
+            }
+            else
+                return null;
+        }
+
+        [SupportedOSPlatform("windows")]
+        public static Image? ImageFromByteArray(byte[] imageBytes)
+        {
+            if (imageBytes != null && imageBytes.Length > 0)
+            {
+                using var ms = new MemoryStream(imageBytes);
+                return Image.FromStream(ms);
+            }
+
+            return null;
+        }
+    }
+}
